af8358dbd45ea9caa6388060cfb38c5e2b408751
Wesley Wei <wesleytianqiwei@gmail.com>
2022-06-08 19:34:29 -0400

    two js files

---
+++ ins.js
@@ -0,0 +1,28 @@
+const context = canvas.getContext('2d');
+
+context.
+{
+    "city": "New York"
+    "name": "John",
+    "name": "John",
+    // // "city": "New Yorkksad"
+    // // "city": "New Yorkksad"
+     "city": "New York"
+    // "city": "New York"
+}
+}
+// "age"akd.dnwiaksjdnwacsd
+wndiw
+AnalyserNodedmsd: 31,
+// "age"akd.dnwiaksjdnwacsd
+wndiw
+AnalyserNodedmsd: 31,
+"age"akd.dnwiaksjdnwacsd
+wndiw
+AnalyserNodedmsd: 31,
+// "age": 31,
+}
+}
+// "name": "John",\
+
+// @ts-check;
